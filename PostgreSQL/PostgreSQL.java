cd44f184f6456af9a02a52014f56185d3c5cb969
DiogoTavaresFeup <32261806+DiogoTavaresFeup@users.noreply.github.com>
2017-11-27 10:56:15 +0000

    Add files via upload
    PostegreSQL conection class: init, disconect.

---
+++ PostgreSQL/PostgreSQL.java
@@ -0,0 +1,61 @@
+package database_conection;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+
+	public class PostgreSQL
+	{
+		
+		 String driver = "org.postgresql.Driver";
+		 Properties props;
+		 Connection con = null;
+		 
+		 
+		 public PostgreSQL(String url, String username, String pass) 
+		 { 
+			 
+			 props = new Properties();
+			 props.setProperty("user", username);
+			 props.setProperty("password", pass);
+			 props.setProperty("ssl", "false");
+			 
+			 try
+			    {
+			        Class.forName(driver);
+			        con =  DriverManager.getConnection(url, props);
+			        System.out.println("Conexão realizada com sucesso.");
+			        System.out.println(con);
+			        
+			    }
+			    catch (ClassNotFoundException ex)
+			    {
+			    	
+			        System.err.print(ex.getMessage());System.out.println("Conexão falhada 1.");
+			    } 
+			    
+			    catch (SQLException e)
+			    {
+			    	
+			        System.err.print(e.getMessage());System.out.println("Conexão falhada 2.");
+			  
+			    }
+
+		 }  
+		 
+		 public void disconect()
+		 {
+			 try {
+				 
+		            con.close();
+		            
+		        } catch (SQLException ex) {
+		            
+		        	System.err.print(ex.getMessage());
+		        	
+		        }
+		 }
+	}
