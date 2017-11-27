4d116ff38b5f9243e5fff9340d4abce4687f5fa6
DiogoTavaresFeup <32261806+DiogoTavaresFeup@users.noreply.github.com>
2017-11-27 11:41:11 +0000

    Update PostgreSQL.java
    Added SQL insert

--- PostgreSQL/PostgreSQL.java
+++ PostgreSQL/PostgreSQL.java
@@ -3,6 +3,7 @@ package database_conection;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Properties;
 
@@ -58,4 +59,42 @@ import java.util.Properties;
 		        	
 		        }
 		 }
+	
+		 
+		 public void regist(String name , String username, String password)
+		 {
+			 try {
+	                PreparedStatement pst = con.prepareStatement("INSERT INTO bcrecord.medico (nome, username,password)values(?,?,?)");
+	               // pst.setString(1, jPasswordField1.getText());
+	              //  pst.setString(2, jTextField6.getText());
+	             pst.setString(1,  name);
+	                pst.setString(2, username);
+		           pst.setString(3, password);
+	                pst.executeUpdate();
+	               // jProgressBar1.setValue(100);
+	              //  JOptionPane.showMessageDialog(rootPane, "Cadastrado com sucesso!");
+	            } catch (SQLException ex) {
+	                //JOptionPane.showMessageDialog(rootPane, "Erro ao cadastrar!\n Erro:"+ex);
+	            	System.err.print(ex.getMessage());
+	            }
+		 }
+		 
+		/* public void login(String username, String password)
+		 {
+			 
+			 String sql = "select * from bcrecord.medico where username=? and password=? ";
+	            PreparedStatement stm = con.prepareStatement(sql);
+	            stm.setString(1, username);
+	            stm.setString(1, password);
+
+	            ResultSet rs = stm.executeQuery();
+
+	            if (rs.next())
+	            {
+	                System.out.println(" Logado");
+	            } else
+	            {
+	                System.out.println("login ou senha incorretos");
+	            }
+		 }*/
 	}
